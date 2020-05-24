9142b7615ba99efe6c1f9a25db8bc4f489b27581
KingBob <52259912+Kingbobs@users.noreply.github.com>
2020-05-24 14:06:01 -0600

    Update main.php

--- src/Zebra/JoinMessage/main.php
+++ src/Zebra/JoinMessage/main.php
@@ -11,23 +11,23 @@ class main extends PluginBase implements Listener
 {
     public function onEnable()
     {
-        $this->getLogger()->info("JoinMessage Plugin ist Aktiviert");
+        $this->getLogger()->info("JoinMessage Plugin");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
     public function onJoin(PlayerJoinEvent $event)
     {
        $player = $event->getPlayer();
        $name = $player->getName();
-    $event->setJoinMessage("\n\n§eHerzlich Willkommen§6 " . $name . "§e auf LightningMC\n\n");
+    $event->setJoinMessage("\n\n§eWelcoem To This Amazing Server§6 " . $name . "§e Here Are The Links\n\n");
 
     }
     public function onQuit(PlayerQuitEvent $event){
         $player = $event->getPlayer();
         $name = $player->getName();
         if ($player->isOP()){
-            $event->setQuitMessage("\n\n§4server §6>§2> §r" . $name . "ist vom LightningMC gegangen!\n\n");
+            $event->setQuitMessage("\n\n§4server §6>§2> §r" . $name . "Has Left The Game Sad To See You GO\n\n");
         } else {
-        $event->setQuitMessage("\n\n§4server §6>§2> §r" . $name . "ist nun nicht mehr auf dem LightningMC Server\n\n"); 
+        $event->setQuitMessage("\n\n§4server §6>§2> §r" . $name . "Has Left The Server Sad To See You Go\n\n"); 
         }
     }
 }
